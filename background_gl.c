732ba1d56d24e708f50c22bd10819849402277f7
AdamSimpson <adambsimpson@gmail.com>
2014-08-26 09:44:16 -0400

    Making background image resolution independent

--- background_gl.c
+++ background_gl.c
@@ -142,8 +142,8 @@ void create_background_texture(background_t *state)
     #endif
     if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
 
-    state->background_width = width;
-    state->background_height = height;
+    state->background_width = 1920/3.2;
+    state->background_height = 1080/1.35;
 
     printf("Background image loaded: %d x %d pixels\n", width, height);
 
